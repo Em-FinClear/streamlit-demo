d299fcedc2f633b33a359f19a85f690a9460c1d3
Em-FinClear <134577663+Em-FinClear@users.noreply.github.com>
2023-06-01 13:44:40 +1000

    Added Zongjie's test demo of streamlit

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,1 +1,66 @@
-st.write("Hello, world!")
+import streamlit as st
+import pandas as pd
+import plotly.express as px
+
+def home_page():
+    # Title
+    st.title("My Streamlit App")
+
+    # Section 1 - Line Chart
+    st.header("Section 1: Line Chart")
+
+    # Assume we have some time series data
+    df_line = pd.DataFrame({
+      'date': pd.date_range(start='1/1/2020', periods=100),
+      'value': (pd.Series(range(100)) + pd.Series(range(100)).cumsum()).tolist()
+    })
+
+    fig1 = px.line(df_line, x='date', y='value', labels={'value':'Values Over Time'})
+    st.plotly_chart(fig1)
+
+    # Section 2 - Pie Chart
+    st.header("Section 2: Pie Chart")
+
+    # Assume we have some categorical data
+    df_pie = pd.DataFrame({
+      'Fruit': ['Apples', 'Bananas', 'Cherries', 'Dates'],
+      'Amount': [15, 30, 8, 6]
+    })
+
+    fig2 = px.pie(df_pie, values='Amount', names='Fruit', title='Fruit Consumption')
+    st.plotly_chart(fig2)
+
+    # Section 3 - Personal Details
+    st.header("Section 3: Personal Details")
+
+    email = st.text_input("Email", "john.doe@example.com")
+    address = st.text_input("Address", "123 Main St, Anytown, USA")
+    shares = st.number_input("Number of shares", 100)
+    age = st.number_input("Age", 30)
+
+    st.write(f"Email: {email}")
+    st.write(f"Address: {address}")
+    st.write(f"Number of Shares: {shares}")
+    st.write(f"Age: {age}")
+
+def map_page():
+    # Title
+    st.title("Map of Sydney")
+
+    map_data = pd.DataFrame({
+        'lat': [-33.8688],
+        'lon': [151.2093]
+    })
+
+    st.map(map_data)
+
+# Pages
+PAGES = {
+    "Home": home_page,
+    "Map": map_page
+}
+
+st.sidebar.title('Navigation')
+selection = st.sidebar.radio("Go to", list(PAGES.keys()))
+page = PAGES[selection]
+page()

37fbca20b8c7b441a636d9e37b8828726a6bfb02
Emlyn Evans <emlyn.evans@finclear.com.au>
2023-06-15 19:54:18 +1000

    Load in private packages on first start

--- app.py
+++ app.py
@@ -77,18 +77,19 @@ try:
 
 # This block executes only on the first run when your package isn't installed
 except ModuleNotFoundError as e:
+    print(f'ERROR RAISED: {e}')
     sleep_time = 30
+    
     dependency_warning = st.warning(
         f"Installing dependencies, this takes {sleep_time} seconds."
     )
-    subprocess.Popen(
-        [
-            f"{sys.executable} -m pip install git+https://{st.secrets.api_keys['github_token']}@github.com/FinClear-Data/toolbox.git",
-        ],
-        shell=True,
-    )
+
+    print(f"{sys.executable} -m pip install git+https://${{github_token}}@github.com/FinClear-Data/toolbox.git")
+
+    subprocess.Popen([f"{sys.executable} -m pip install git+https://${{github_token}}@github.com/FinClear-Data/toolbox.git"], shell=True)
 
     # wait for subprocess to install package before running your actual code below
+    
     time.sleep(sleep_time)
     # remove the installing dependency warning
     dependency_warning.empty()
